986cde242276c69e4d9ac7ab179a677bc4cd2db6
Lololochen <2978471077@qq.com>
2017-08-29 18:06:44 +0800

    Initial Commit

---
+++ TestAudio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestAudio
+//
+//  Created by yy on 2017/8/29.
+//  Copyright © 2017年 yy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
